1efa588d624528600c8cc40be640b5e4a03c42c7
ChaitaliGondhalekar <ChaitaliGondhalekar@users.noreply.github.com>
2014-05-11 18:37:07 -0400

    Update stack.h

--- src/stack.h
+++ src/stack.h
@@ -1,21 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #define CAPACITY 20
 
 typedef struct STACK{
-	// give the invariants and describe the stack abstraction here
-	int array[CAPACITY];
-	int tos;//top of stack
-}stack;
-// it's actually a good idea to define a destructor for a new datatype, even if it is trivial.
-// It's call gives you a way to tell the reader that the data structure will no longer be used.
-// Moreover, it gives you more flexibility to change the representation later, e.g. to use the heap,
-// without breaking the already-written client code.
-void initStack(stack *);
-int pop(stack *);
-void push(stack * , int);
-bool isStackEmpty(stack *);
-void printStack(stack *);
-void clearStack(stack *);
+
+	int array[CAPACITY];//capacity of Stack
+	int tos;//length of stack
+}Stack;
+
+
+/**
+initializes the Stack
+*/
+void initStack(Stack *);
+
+/**
+@param - pointer to the Stack
+removes and returns the element at top of stack
+*/
+int pop(Stack *);
+
+/**
+@param - pointer to the Stack and the element
+pushes the element at top of stack
+*/
+void push(Stack * , int);
+
+/**
+@param - pointer to the Stack
+returns true if stack is empty
+false otherwise
+*/
+bool isStackEmpty(Stack *);
+
+/**
+@param - pointer to the Stack
+prints out the entire stack
+*/
+void printStack(Stack *);
