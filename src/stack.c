de9a47d078870bf94732895d5f24dd4cc1db89b2
ChaitaliGondhalekar <ChaitaliGondhalekar@users.noreply.github.com>
2014-05-11 18:36:24 -0400

    Update stack.c

--- src/stack.c
+++ src/stack.c
@@ -1,53 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "stack.h"
 
-#define btoa(isStackEmpty) ((isStackEmpty)?"true":"false")
-// there is no reason to initialize the data in the stack
-// use half-open intervals for data structures, i.e. tos==0 should indicate an empty stack
-void initStack(stack * stackHead){
-	stackHead->tos =-1;
-	for(int i=0;i<CAPACITY;i++){
-		stackHead->array[i]= -1;
-	}
+// #define btoa(isStackEmpty) ((isStackEmpty)?"true":"false")
+
+void initStack(Stack * stack){
+
+	stack->tos = 0;//length of the stack
 }
 
-int pop(stack * stackHead){
+int pop(Stack * stack){
 
 	int top;
-	if(stackHead->tos == -1)//empty stack!
+	if(stack->tos == 0)//empty stack!
 		return -1;
-	top = stackHead->array[stackHead->tos];
-	stackHead->array[stackHead->tos] = -1;
-	stackHead->tos--;
-	return top;
+	return stack->array[--(stack->tos)];
 }
 
-void push(stack * stackHead, int ID) {
+void push(Stack * stack, int ID) {
 
-	if(stackHead->tos == CAPACITY)
-		printf("Stack full");
-	else{
-		++(stackHead->tos);
-		stackHead->array[stackHead->tos] = ID;
+	if(stack->tos == CAPACITY)
+		printf("Stack full\n");
+	else
+		stack->array[(stack->tos)++] = ID;
+}
+
+void printStack(Stack * stack){
+
+	int i=0;
+	while(i!= stack->tos)
+	{
+		printf("Element %d: %d\n",i,stack->array[i]);
+		i++;
 	}
 }
 
-void printStack(stack * stackHead){
+bool isStackEmpty(Stack * stack){
 
-	printf("In print\n");
-	for(int i=0; i<CAPACITY;i++){
-		if(stackHead -> array[i] != -1)
-		printf("Element %d: %d\n",i,stackHead->array[i]);
-	}
-}
-
-bool isStackEmpty(stack * stackHead){
-
-	if(stackHead->tos == -1)
+	if(stack->tos == 0)
 		return true;
 	return false;
-}
-
-void clearStack(stack * stackHead)
-{
-	initStack(stackHead);
 }
