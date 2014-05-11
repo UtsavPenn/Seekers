c095282d1ac479dbe6c850690504900f519a716a
Shallav <shallav@seas.upenn.edu>
2014-05-11 17:45:32 -0400

    Motor code made awesome

--- src/motor.c
+++ src/motor.c
@@ -1,205 +1,154 @@
-g/*
+/*
   1. Creates a graph structure representing the environment.
   2. Move the bot in a given direction for given amount of time.
   @Author: Utsav Mehta and Shallav Varma
 */
-
 #include <stdio.h>
 #include <bcm2835.h>
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <time.h> 
+#include <sys/types.h> 
 #include "motor.h"
 
 #define MOTORA RPI_GPIO_P1_12
 #define MOTORB RPI_GPIO_P1_11
 
-graph area;
-
 extern orientation lastTurn;
-
-struct motor_params{
-  int pin;
-  float duty;
-  };
-
-struct motor_params motorleft,motorright;
-
-// if the purpose of the function is to create a graph, it should be a function returning a graph
-// program what you mean
-
-/*
-  It creates the graph structure using Graph API.
-*/
-void run ()
-{
-	lastTurn = Straight;
-    int i = 0;
-    // Creates a linked list with number of nodes = number of times the loop executes.
-  
-    graphInit(&area);
-    while(i < 9)
-    {
-        area.addNode(&area);
-        i++;
-    }
-    // Adding edges between various nodes of graph.
-    area.addEdge(&area, 0, 1, 3, Right);
-    area.addEdge(&area, 1, 2, 3, Right);
-    area.addEdge(&area, 2, 5, 3, Straight);
-    area.addEdge(&area, 5, 4, 3, Left);
-    area.addEdge(&area, 4, 3, 3, Left);
-    area.addEdge(&area, 1, 4, 3, Straight);
-    area.addEdge(&area, 0, 3, 3, Straight);
-    area.addEdge(&area, 3, 6, 3, Straight);
-    area.addEdge(&area, 6, 7, 3, Right);
-    area.addEdge(&area, 7, 8, 3, Right);
-    area.addEdge(&area, 4, 7, 3, Straight);
-    area.addEdge(&area, 5, 8, 3, Straight);
-
-    print(&area);
-    
-  }
-
 /* 
   Initialize the left and the right motors by setting the 
   corresponding GPIO pins as outputs.
 */
-void init_motors(){
-   motorleft.pin = MOTORA;
-   motorright.pin = MOTORB;
+
+void motor_init(bot* car){
+   car->motorleft.pin = MOTORA;
+   car->motorright.pin = MOTORB;
+  
    if(!bcm2835_init())
     return ;
-
+  
   bcm2835_gpio_fsel(MOTORA, BCM2835_GPIO_FSEL_OUTP);
   bcm2835_gpio_fsel(MOTORB, BCM2835_GPIO_FSEL_OUTP);
+ 	
+  car->turnRight = &turnRight; 
+  car->turnLeft = &turnLeft;
+  car->driveStraight = &driveStraight;
+  car->startmotors = &startmotors;
+  car->rotate360 = &rotate360;
+  car->orient = &orient;
 }
 
 /*
   The driver for motors.
 */
 void *drivemotors(void *arg){
+ time_t now;
+ struct tm *tm;
+ int entryTime;
+ now = time(0);
+ tm = localtime (&now);
+ motor* mot = (motor*)arg; 
+ entryTime = tm->tm_sec;
 
- struct  motor_params* motor = (struct motor_params*)arg;
-
- 
- while(1){
-   bcm2835_gpio_write(motor->pin,HIGH);
-   bcm2835_delayMicroseconds(motor->duty*200);
-   bcm2835_gpio_write(motor->pin,LOW);
-   bcm2835_delayMicroseconds((100-motor->duty)*200);
- 
+ while(tm->tm_sec <= entryTime + mot->ontime){
+   printf("Entry time %d\n",tm->tm_sec);
+   bcm2835_gpio_write(mot->pin,HIGH);
+   bcm2835_delayMicroseconds(mot->duty*200);
+   bcm2835_gpio_write(mot->pin,LOW);
+   bcm2835_delayMicroseconds((100-mot->duty)*200);
+   now = time(0);
+   tm = localtime (&now);
+   
   } 
 }
-
 /*
   Runs the bot in straight direction for a given amount of time.
   @args
   time : time for which bot should drive straight. 
 */
-void driveStraight(int timer){
-  printf("Going Straight\n");
+
+
+void startmotors(bot* car){
+  time_t now;
+  struct tm *tm;
   pthread_t lth ,rth;
-  motorright.duty = 3.75;
-  motorleft.duty = 11.25;
+  pthread_create(&rth,NULL,drivemotors,(void*)&car->motorright);
+  pthread_create(&lth,NULL,drivemotors,(void*)&car->motorleft);
   
-  //Starting motor threads   
-  pthread_create(&rth,NULL,drivemotors,(void*)&motorright);
-  pthread_create(&lth,NULL,drivemotors,(void*)&motorleft);
-    printf("Threads created in drive straight.: %d \n",timer);  
-  sleep(timer);
-  
-  pthread_cancel(rth);
-  pthread_cancel(lth);
   pthread_join(rth,NULL);
   pthread_join(lth,NULL);
-	printf("Threads cancelled in drive straight. \n");  
+}
+
+
+void driveStraight(bot* car, int timer){
+  printf("Going Straight\n");
+  car->motorright.duty = 3.75;
+  car->motorleft.duty = 11.25;
+  car->motorleft.ontime = timer;
+  car->motorright.ontime = timer;
+  startmotors(car);
 }
 
 /*
   Rotates the bot in right direction.
 */
-void turnRight(){
+void turnRight(bot* car){
   printf("Turning Right\n");
-  pthread_t lth ,rth;
-  motorright.duty = 7.25;
-  motorleft.duty = 11.25;
-  
-  pthread_create(&rth,NULL,drivemotors,(void*)&motorright);
-  pthread_create(&lth,NULL,drivemotors,(void*)&motorleft);
-  
-  usleep(1370000);
-  
-  pthread_cancel(rth);
-  pthread_cancel(lth);
-  pthread_join(rth,NULL);
-  pthread_join(lth,NULL);
-  
+  car->motorright.duty = 7.25;
+  car->motorleft.duty = 11.25;
+  car->motorleft.ontime = 1.37;
+  car->motorright.ontime = 1.37;
+  startmotors(car);
 }
 
 /*
   Rotates the bot in left direction.
 */
-void turnLeft(){
+void turnLeft(bot* car){
   printf("Turning Left\n");
-  pthread_t lth ,rth;
-  motorright.duty =7.25;
-  motorleft.duty = 3.75;
-  
-  pthread_create(&rth,NULL,drivemotors,(void*)&motorright);
-  pthread_create(&lth,NULL,drivemotors,(void*)&motorleft);
-  
-  usleep(1370000);
-  pthread_cancel(rth);
-  pthread_cancel(lth);
-  pthread_join(rth,NULL);
-  pthread_join(lth,NULL);
+  car->motorright.duty =7.25;
+  car->motorleft.duty = 3.75;
+  car->motorleft.ontime = 1.37;
+  car->motorright.ontime = 1.37;
+  startmotors(car); 
   
 }
 
 /*
   Rotates the bot 360 degrees.
 */
-void rotate360(){
-   pthread_t lth ,rth;
-  motorright.duty = 7.25;
-  motorleft.duty = 11.25;
-  
-  pthread_create(&rth,NULL,drivemotors,(void*)&motorright);
-  pthread_create(&lth,NULL,drivemotors,(void*)&motorleft);
-  
-  usleep(1330000*4);
-  
-  pthread_cancel(rth);
-  pthread_cancel(lth);
-  pthread_join(rth,NULL);
-  pthread_join(lth,NULL);
+void rotate360(bot* car){
+  car->motorright.duty = 7.25;
+  car->motorleft.duty = 11.25;
+  car->motorleft.ontime = 5.48;
+  car->motorright.ontime = 5.48;
+  startmotors(car);
    
 }
 
 /*
   Orients the bot wrt reference direction.
 */
-void orient()
+void orient(bot* car)
 {
 	printf("\n Orienting itself.: %d \n",lastTurn); 
       sleep(1);
       switch(lastTurn)
       {
               case Right:
-                      turnLeft();
+                      car->turnLeft();
                       sleep(1);
                       break;
               case Back:
-                      turnRight();
-			                sleep(1);
-                      turnRight();
+                      car->turnRight();
+		      sleep(1);
+                      car->turnRight();
                       sleep(1);
                       break;
               case Left:
-                      turnRight();
+                      car->turnRight();
                       sleep(1);
                       break;
               default:
@@ -210,31 +159,31 @@ void orient()
 /*
   Bot moves in the given direction for a given amount of time.  
 */
-void moveCar(orientation direction, int timer){
+void moveCar(bot car, orientation direction, int timer){
   // Pre - condition: Bot can face in any direction.
   orient(); 
   // Bot faces in the reference direction here.
   switch(direction){
   	case Straight:
-  	  driveStraight(timer);
+  	  car.driveStraight(timer);
   	break;
   	case Right:
-  	  turnRight();
+  	  car.turnRight();
 	driveStraight(timer);
   	break;
   	case Left:
-  	  turnLeft();
-	driveStraight(timer);
+  	  car.turnLeft();
+	  car.driveStraight(timer);
   	break;
 	case Back:
-		turnRight();
+		car.turnRight();
 		sleep(1);
-		turnRight();
-		driveStraight(timer);
+		car.turnRight();
+		car.driveStraight(timer);
 		break;
   	default:
-  	  motorright.duty = 7.25;
-  	  motorleft.duty = 7.25;
+  	  car.motorright.duty = 7.25;
+  	  car.motorleft.duty = 7.25;
   	  break; 
   }
   return ;
--- src/motor.h
+++ src/motor.h
@@ -1,8 +1,41 @@
-#include "graph.h"
 
-// why are you writing what looks like C in a C++ project?
-void moveCar(orientation, int);
-void init_motors();
-void run();
-void rotate360();
-void turnRight();
+/*struct which contains the attributes required for moving the motor*/
+typedef struct motor{
+  int pin;
+  float duty;
+  float timer;
+  }motor;
+
+/*struct which contains the function pointers necessary for the movement of the bot*/
+typedef struct bot {
+  motor motorright;
+  motor motorleft; 
+  
+  void (*turnRight)(struct bot* self);
+  void (*turnLeft)(struct bot *self);
+  void (*driveStraight)(struct bot *self, int timer);
+  void (*rotate360)(struct bot *self);
+  void (*startmotors)(struct bot *self, int timer);
+  void (*orient)(struct bot* self);
+}bot;
+
+/*initialises the motor pins and aaigns the function pointers to specific functions*/
+void motor_init(bot*);
+
+/*rotates the bot 360*/
+void rotate360(bot*);
+
+/*turns the bot right*/
+void turnRight(bot*);
+
+/*turns the bot leftt*/
+void turnLeft(bot*);
+
+/*moves the bot Straight*/
+void driveStraight(bot*, int);
+
+/*starts the motors*/
+void startmotors(bot*,int);
+
+/*an interface function for dfs code to move the bot according to the graph*/
+void moveCar(bot, orientation, int);
